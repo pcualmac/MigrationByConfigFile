f299941eaed1d40abbcac8b75235a6518c1b886f
Calogero Pumpignano <pumpignano@gmail.com>
2020-04-14 09:50:20 +0100

    add files

---
+++ src/MigrationByConfigFileServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace jerry\MigrationByConfigFile;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class MigrationByConfigFileServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/config/MigrationByConfigFile.php',
+            'MigrationByConfigFile'
+        );
+        $this->publishes([
+            __DIR__ . '/config/MigrationByConfigFile.php' => config_path('MigrationByConfigFile.php'),
+        ]);
+        $this->loadMigrationsFrom(config('MigrationByConfigFile.path'));
+    }
+}
